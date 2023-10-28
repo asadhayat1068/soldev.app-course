c130207dd483faf5a121b3abd4a102e84e2d822e
Asad Hayat <asadhayat.1068@gmail.com>
2023-10-28 19:00:40 +0500

    Added pagination

--- demos/app/serialize-instruction-data/MovieList.tsx
+++ demos/app/serialize-instruction-data/MovieList.tsx
@@ -8,20 +8,47 @@ function MovieList({ ProgramId }: { ProgramId: string }) {
   const { connection } = useConnection();
   const { publicKey } = useWallet();
   const [movies, setMovies] = useState<(Movie | null)[]>([]);
+  const [page, setPage] = useState(0);
+  const [maxPage, setMaxPage] = useState(0);
+  const itemsPerPage = 10;
   useEffect(() => {
-    getReviews();
+    getReviews(0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [publicKey, connection]);
 
-  const getReviews = async () => {
+  const getReviews = async (pageNumber: number) => {
     const accounts = await connection.getProgramAccounts(
-      new PublicKey(ProgramId)
+      new PublicKey(ProgramId),
+      {
+        dataSlice: {
+          offset: 0,
+          length: 0,
+        },
+      }
     );
-    const _movies: (Movie | null)[] = accounts.map(({ account }) => {
-      return Movie.deserialize(account.data);
+    const accountKeys = accounts.map(({ pubkey }) => pubkey);
+    setMaxPage(Math.ceil(accountKeys.length / itemsPerPage));
+    const startIndex = pageNumber * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const paginatedKeys = accountKeys.slice(startIndex, endIndex);
+    const moviesData = await connection.getMultipleAccountsInfo(paginatedKeys);
+    const _movies: (Movie | null)[] = moviesData.map((account) => {
+      return Movie.deserialize(account?.data);
     });
     setMovies(_movies);
-    console.log(_movies);
+  };
+  const nextPage = () => {
+    if (page < maxPage) {
+      setPage(page + 1);
+      getReviews(page + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+      getReviews(page - 1);
+    }
   };
 
   return (
@@ -29,6 +56,13 @@ function MovieList({ ProgramId }: { ProgramId: string }) {
       <header className="text-3xl py-5">
         <p className="text-center">Movie Reviews</p>
       </header>
+      <section className="flex flex-col">
+        <div className="text-center my-4">
+          <button onClick={prevPage}> &lt; Prev </button>
+          <span> &nbsp;&nbsp; {page + 1} &nbsp;&nbsp;</span>
+          <button onClick={nextPage}> Next &gt; </button>
+        </div>
+      </section>
       <section className="flex flex-col">
         {movies.map((movie, i) => (
           <div
